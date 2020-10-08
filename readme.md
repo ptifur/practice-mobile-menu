7f4c866f0292f5a24a32f0711a9e43cc9abb426f
Y <ptifur@gmail.com>
2020-10-08 06:29:52 +0300

    mostly complete

---
+++ readme.md
@@ -0,0 +1,5 @@
+## Responsive menu snippet
+
+May come in handy at some point
+
+Based on this dev.to post <https://dev.to/ljcdev/easy-hamburger-menu-with-js-2do0>
--- script/script.js
+++ script/script.js
@@ -1,11 +1,19 @@
+// responsive veiw menu background
 const menu = document.querySelector('.mobile_menu')
 const menuButton = document.querySelector('.menu_button')
+const menuLinks = document.querySelectorAll('.mobile_menu ul li a')
+const body = document.querySelector('body')
 
+// menu button
 menuButton.addEventListener('click', toggleMenu)
+// menu items
+menuLinks.forEach((menuLink) => menuLink.addEventListener('click', toggleMenu))
 
 function toggleMenu() {
     menu.classList.toggle('mobile_menu_show')
+    // smooth transition
     if (!menu.classList.contains('mobile_menu_transition')) {
-        menu.classList.add('mobile_menu_transition')
-    }
+        menu.classList.add('mobile_menu_transition')}
+    // prevent scrolling when menu is open
+    body.classList.toggle('lock')
 }
